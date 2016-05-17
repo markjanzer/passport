c463565759a0b0fb635baee03d99bec62e62bc98
markjanzer <mark.janzer@gmail.com>
2016-05-17 16:19:14 -0700

    Connect front end to back. Add Content-type application/json

--- app.rb
+++ app.rb
@@ -5,11 +5,19 @@ pvs = PiranhaViewServer.new
 set :port, 3000
 set :environment, :production
 
+before do
+  headers['Access-Control-Allow-Origin'] = "http://localhost:3333"
+  headers["Content-Type"] = "application/json"
+  puts headers
+  puts "*" * 40
+end
+
 get '/' do
   p "Server is running"
 end
 
 post '/api/timeslots' do
+  p "POST TIMESLOTS"
   p params
   timeslot = pvs.new_timeslot(params[:timeslot][:start_time], params[:timeslot][:duration])
   p timeslot.to_hash.to_json
@@ -17,20 +25,23 @@ post '/api/timeslots' do
 end
 
 get '/api/timeslots' do
-  p params
-  p pvs.timeslots[0].get_date_as_string
-  p params[:date]
-  p pvs.timeslots[0].get_date_as_string == params[:date]
-  timeslots_of_date = pvs.timeslots.select do |timeslot|
-    timeslot.get_date_as_string == params[:date]
+  p "GET TIMESLOTS"
+  if pvs.timeslots.empty?
+    return [].to_json
+  else
+    p pvs.timeslots[0].get_date_as_string == params[:date]
+    timeslots_of_date = pvs.timeslots.select do |timeslot|
+      timeslot.get_date_as_string == params[:date]
+    end
+    p timeslots_of_date
+    timeslots_as_hashes = pvs.get_collection_as_hashes(timeslots_of_date)
+    p timeslots_as_hashes.to_json
+    return timeslots_as_hashes.to_json
   end
-  p timeslots_of_date
-  timeslots_as_hashes = pvs.get_collection_as_hashes(timeslots_of_date)
-  p timeslots_as_hashes.to_json
-  return timeslots_as_hashes.to_json
 end
 
 post '/api/boats' do
+  p "POST BOATS"
   p params
   boat = pvs.new_boat(params[:boat][:capacity], params[:boat][:name])
   p boat.to_hash.to_json
@@ -39,6 +50,7 @@ post '/api/boats' do
 end
 
 get '/api/boats' do
+  p "GET BOATS"
   p params
   p pvs.get_collection_as_hashes(pvs.boats).to_json
   return pvs.get_collection_as_hashes(pvs.boats).to_json
